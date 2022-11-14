bcafcac16da30aaf68b125102af0e159bed10969
tunatuna2005 <tuna200538@gmail.com>
2022-11-14 14:05:39 +0900

    done

--- 배열02_20119이현준.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    char a[20];
-    printf("문자열 입력?: ");
-    scanf("%s", a);
-    printf("입력 문자열 출력!: %s\n", a);
-    printf("2번째 문자 출력!: %c\n", a[1]);
-}
--- 배열04_20119이현준.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    char c[][10]={"school", "subject", "program", "study"};
-    int a;
-    for(int i=0;i<4;i++){
-        printf("%s \n", c[i]);
-    }
-    return 0;
-}
